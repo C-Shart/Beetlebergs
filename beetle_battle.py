402a621282fa2771efe2cfa3ad89ad749769c534
Aaron B <C-Shart@users.noreply.github.com>
2023-04-03 15:13:39 -0700

    Making changes based on PR

--- beetle_battle.py
+++ beetle_battle.py
@@ -1,7 +1,3 @@
-# Basic arcade program using objects
-# Displays a white window with a blue circle in the middle
-
-# Imports
 import arcade
 
 # Constants
@@ -9,51 +5,39 @@ SCREEN_WIDTH = 1280
 SCREEN_HEIGHT = 720
 SCREEN_TITLE = "Welcome to Arcade"
 
-# Scaling constants
+BEETLE_SPRITE_PATH_GREEN = "Assets/Sprites/beetle_GRN.png"
+BEETLE_SPRITE_PATH_RED = None
 BEETLE_SCALING = .65
 
-# Classes
 class BeetleBattle(arcade.Window):
     def __init__(self):
 
         # Call the parent class constructor
         super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
         
-        # Beetle sprites in this variable
-        self.beetle_list = None
-        self.beetle_sprite = None
-
-        # Background image stored in this variable
+        self.beetle_sprite_list = None
+        self.beetle_prototype = None
         self.background = None
 
-        # Set the background window
         arcade.set_background_color(arcade.color.WHITE)
 
     def setup(self):
-        # Create the background
         self.background = arcade.load_texture("Assets/Images/octagon.png")
 
-        # Create sprite lists
-        self.beetle_list = arcade.SpriteList()
+        self.beetle_sprite_list = arcade.SpriteList()
 
         # Set up the beetle and place it at these coordinates
-        beetle_sprite_source = "Assets/Sprites/beetle_GRN.png"
-        self.beetle_sprite = arcade.Sprite(beetle_sprite_source, BEETLE_SCALING)
-        self.beetle_sprite.center_x = 70
-        self.beetle_sprite.center_y = 65
-        self.beetle_list.append(self.beetle_sprite)
+        self.beetle_prototype = arcade.Sprite(BEETLE_SPRITE_PATH_GREEN, BEETLE_SCALING)
+        self.beetle_prototype.center_x = 70
+        self.beetle_prototype.center_y = 65
+        self.beetle_sprite_list.append(self.beetle_prototype)
 
     def on_draw(self):
-        # Clear the screen
         arcade.start_render()
-
-        # Start drawing
         arcade.draw_lrwh_rectangle_textured(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, self.background)
-        self.beetle_list.draw()
+        self.beetle_sprite_list.draw()
 
 
-
-# Main code entry point
 if __name__ == "__main__":
     app = BeetleBattle()
     app.setup()
