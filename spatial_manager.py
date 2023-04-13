a99f24611e8d3046c5b7b02b8f6c0eb258573d3e
Aaron <C-Shart@users.noreply.github.com>
2023-04-12 23:15:13 -0700

    Adding SpatialManager class scaffolding

---
+++ spatial_manager.py
@@ -0,0 +1,12 @@
+class SpatialManager:
+    def __init__(self):
+
+        pass
+
+    def on_update(self, delta_time):
+        # TODO: Recalculates buckets, adds sprites and spritelists to track
+        pass
+
+    def get_nearby(self):
+        # TODO: Called after on_update has ran to get objects nearby a certain x, y
+        pass
